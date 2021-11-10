93b4e42f0a05b5c519aee68c843709281ec65a15
PavaniKempula <pavanikempula1998@gmail.com>
2021-11-11 01:48:21 +0530

    ADDED : Welcome to Cab Invoice Generator Program

---
+++ src/main/java/com/bridgelabz/cabinvoicegenerator/CabInvoiceGenerator.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.cabinvoicegenerator;
+
+public class CabInvoiceGenerator {
+    public static void main(String[] args) {
+        // Diplaying Welcome Message
+        System.out.println("Welcome to Cab Invoice Generator Program");
+    }
+}
